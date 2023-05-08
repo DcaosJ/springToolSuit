5ae383fc13804249a3c17099ef5a127636c0141a
jd <jd@juan-aldapro>
2023-05-08 09:08:30 +0200

    primerComit

---
+++ warhammer40k/src/main/java/dam/thymleft/warhammer40k/App.java
@@ -0,0 +1,13 @@
+package dam.thymleft.warhammer40k;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
